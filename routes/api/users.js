32261bcdc33c4a68434b56673e0fb0c0828e6a7a
Jaspreet Singh <jass.waraich@icloud.com>
2018-12-05 12:17:51 -0500

    Added Validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 // Load User Model
 const User = require("../../models/User");
 
@@ -24,9 +28,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // It's a mongoose method,, finding if the e-mail already exists with which the user is regstering
 // We can use promises or callbacks with mongoose and here we are using promises
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size of the avatar
@@ -58,6 +70,12 @@ router.post("/register", (req, res) => {
 // @desc   Login User/ Returning a JWT Token
 // @acess  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email; //we are using body parser
   const password = req.body.password;
 
@@ -65,7 +83,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -88,7 +107,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
---
+++ validation/is-empty.js
@@ -0,0 +1,8 @@
+// Arrow function to check empty status
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
