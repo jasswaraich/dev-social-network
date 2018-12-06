0ceeccf71e6b88e6bdcb91e8b163aca40faec59e
Jaspreet Singh <jass.waraich@icloud.com>
2018-12-05 20:57:41 -0500

    updated profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -2,3 +2,55 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId  // Associate the user's info with the profile by it's ID 
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+
+
+  },
+  company: {
+    type: String
+
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type:String
+
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills:{
+    type: [String] //An array of strings
+    required: true  
+  },
+  bio: {
+    type: String
+
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+
+    },
+    company:{
+
+    }
+  ]
+  }    
+
+});
