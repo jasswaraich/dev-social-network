01aec0bed0fba1781fa7ada634ff84bc449ebe0a
Jaspreet Singh <jass.waraich@icloud.com>
2018-12-06 10:44:04 -0500

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model
+
+const Profile = require("../../models/Profile");
+
+// Load User Model
+const User = require("../../models/User");
 
 // @route  GET api/profile/test
 // @desc   Tests profile route
-// @acess  Public 
+// @acess  Public
 
 // We can use any method get/post/put
-// This will output json , will automatically serve status of 200('everything is ok') 
+// This will output json , will automatically serve status of 200('everything is ok')
 // This will refer to 'localhost:port/api/profile/test as we have already referenced it in server.js'
-router.get('/test',(req, res) => res.json({msg: "Profile Works"})); 
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route  GET api/profile/:id
+// @desc   Tests profile route
+// @acess  Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
 module.exports = router; // for server.js to pick it up
