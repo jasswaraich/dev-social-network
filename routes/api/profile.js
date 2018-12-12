e8bb601b117a7e373d9007cfc3cbd0edbb9bbe48
jasswaraich <jass.waraich@icloud.com>
2018-12-11 19:55:54 -0500

    updated profile route with authentication

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,9 +19,25 @@ const User = require("../../models/User");
 // This will refer to 'localhost:port/api/profile/test as we have already referenced it in server.js'
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
-// @route  GET api/profile/:id
-// @desc   Tests profile route
-// @acess  Public
-router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+// @route  GET api/profile/
+// @desc   Get current user profile
+// @acess  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router; // for server.js to pick it up
